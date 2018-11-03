bded8d3a31935250beac2fdb96110781efbfdaf4
Константин Юрин <yurinkonstantin@mail.ru>
2018-11-03 19:27:39 +0300

    Добавьте файлы проекта.

---
+++ NewsAppUWP/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace NewsAppUWP
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "Glavnoe":
+                   ContentFrame.Navigate(typeof(PivotPageGlavnoe));
+                    break;
+
+                case "Rus":
+                    ContentFrame.Navigate(typeof(PivotPageRus));
+                    break;
+
+                case "World":
+                    // ContentFrame.Navigate(typeof(GamesPage));
+                    break;
+
+                case "Sport":
+                    //  ContentFrame.Navigate(typeof(MusicPage));
+                    break;
+
+                case "Econom":
+                    //  ContentFrame.Navigate(typeof(MyContentPage));
+                    break;
+                case "Zac":
+                    //  ContentFrame.Navigate(typeof(MyContentPage));
+                    break;
+            }
+        }
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+           // On_BackRequested();
+
+        }
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                // ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
+            }
+        }
+    }
+}
